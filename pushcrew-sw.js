ff58b24c5494c94614b7da24dd049e36e5a402c2
MinMax0 <34135592+MinMax0@users.noreply.github.com>
2018-04-16 16:28:23 +0530

    Update pushcrew-sw.js

--- pushcrew-sw.js
+++ pushcrew-sw.js
@@ -1,2 +1,2 @@
 var version = 6;
-importScripts('https://cdn.pushcrew.com/sw/968e99d3230e28a07ad245192832ee7c.js');
+importScripts('https://cdn.pushcrewstaging.com/sw/968e99d3230e28a07ad245192832ee7c.js');
